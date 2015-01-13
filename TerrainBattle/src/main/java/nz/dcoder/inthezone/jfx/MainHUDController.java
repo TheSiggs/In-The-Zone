1dfe7405d839832ff6f4b9f9084013fcf1886f51
Sam Siggs <samsiggs00@gmail.com>
2015-01-13 21:38:06 +1300

    Added Health Bar (WIP). Removed "loading_screen.fxml". Moved Tims button.

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/jfx/MainHUDController.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/jfx/MainHUDController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.paint.Color;
 import nz.dcoder.inthezone.Main;
 
@@ -24,31 +25,49 @@ import nz.dcoder.inthezone.Main;
  */
 public class MainHUDController implements Initializable {
 
-	public static Main app;
-	@FXML
-	public Canvas canvas;
-	/**
-	 * Initializes the controller class.
-	 */
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		// TODO
-		GraphicsContext ctx = canvas.getGraphicsContext2D();
-		ctx.setFill(new Color(1.0, 0.0, 0.0, 1.0));
-		ctx.rect(10, 10, 50, 50);
-		ctx.fill();
-	}	
-	
-	@FXML
-	private void handleButtonAction(ActionEvent event) {
-		System.out.println("You clicked me!");
-		app.enqueue(new Callable<Main>() {
+    public static Main app;
+    @FXML
+    public Canvas canvas;
 
-			@Override
-			public Main call() throws Exception {
-				app.someMethod();
-				return app;
-			}
-		});
-	}
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        GraphicsContext ctx = canvas.getGraphicsContext2D();
+        ctx.setFill(new Color(1.0, 0.0, 0.0, 1.0));
+        ctx.rect(10, 10, 50, 50);
+        ctx.fill();
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        app.enqueue(new Callable<Main>() {
+
+            @Override
+            public Main call() throws Exception {
+                app.someMethod();
+                return app;
+            }
+        });
+    }
+    
+    @FXML
+    private ProgressBar health;
+
+    @FXML
+    private void healthUp(ActionEvent event) {
+        health.setProgress(health.getProgress() + 0.1);
+
+        System.out.println("Health Up");
+    }
+
+    @FXML
+    private void healthDown(ActionEvent event) {
+        health.setProgress(health.getProgress() - 0.1);
+
+        System.out.println("Health Down");
+    }
 }

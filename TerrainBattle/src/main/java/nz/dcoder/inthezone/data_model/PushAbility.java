22db0a9e34ca607ec431e94e0655365ac442cff5
Callum Lowcay <callum@callumscode.com>
2015-02-13 14:09:34 +1300

    data_model: implement push effect

---
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/data_model/PushAbility.java
@@ -0,0 +1,75 @@
+package nz.dcoder.inthezone.data_model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import nz.dcoder.inthezone.data_model.pure.EffectName;
+import nz.dcoder.inthezone.data_model.pure.Position;
+
+public class PushAbility extends Ability {
+	public static EffectName effectName = new EffectName("push");
+
+	public PushAbility(AbilityInfo info) {
+		super(effectName, info);
+	}
+
+	private Position getPushDestination(
+		Position agentPosition, Position target
+	) {
+		int dx = target.x - agentPosition.x;
+		int dy = target.y - agentPosition.y;
+
+		if (dx > 0) return target.add(new Position(1, 0));
+		else if (dx < 0) return target.add(new Position(-1, 0));
+		else if (dy > 0) return target.add(new Position(0, 1));
+		else if (dy < 0) return target.add(new Position(0, -1));
+		else return agentPosition;
+	}
+
+	@Override
+	public Collection<Position> getAffectedArea(
+		Position agentPosition, Position target, Battle battle
+	) {
+		List<Position> affected = new ArrayList<Position>();
+		affected.add(target);
+		affected.add(getPushDestination(agentPosition, target));
+		return affected;
+	}
+
+	@Override
+	public void applyEffect(CanDoAbility agent, Position target, Battle battle) {
+		if (canApplyEffect(agent, target, battle)) {
+			Position apos = agent.getPosition();
+			Position pushDestination = getPushDestination(apos, target);
+			battle.getObjectAt(target).position = pushDestination;
+			battle.getCharacterAt(apos).position = target;
+		}
+	}
+
+	@Override
+	public boolean canApplyEffect(
+		CanDoAbility agent, Position target, Battle battle
+	) {
+		Position apos = agent.getPosition();
+
+		// check range
+		if (Math.abs(apos.x - target.x) != 1 || Math.abs(apos.y - target.y) != 1) {
+			return false;
+		}
+
+		// check that there is something we can push
+		BattleObject obj = battle.getObjectAt(target);
+		if (obj == null || !obj.isPushable) {
+			return false;
+		}
+
+		// check the push is not blocked
+		Position pushDestination = getPushDestination(apos, target);
+		if (battle.getOccupiedPositions().contains(pushDestination)) {
+			return false;
+		}
+
+		return true;
+	}
+}
+

2c4b137343459d666f364efb8d0ba829a8e7be8c
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-02-15 15:20:58 +1300

    Fixed issue #8 - Removed redundant AI code.

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/Main.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/Main.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.function.Consumer;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javax.vecmath.Point2i;
@@ -37,6 +38,7 @@ import nz.dcoder.ai.astar.BoardNode;
 import nz.dcoder.ai.astar.BoardState;
 import nz.dcoder.ai.astar.Tile;
 import nz.dcoder.inthezone.concurrent.RunnableWithController;
+import nz.dcoder.inthezone.data_model.Turn;
 import nz.dcoder.inthezone.objects.CharacterState;
 
 /**

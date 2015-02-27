9c1a886ed21f2780d3451497a54dcef24eade2d8
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-02-27 23:53:55 +1300

    Loadable CSV file loading models dynamically.
    When loading a scene.csv file with filename,
    x, y, z coordinates one can define where a
    model is located.

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/BlenderRender.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/BlenderRender.java
@@ -11,9 +11,11 @@ import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  *
@@ -23,6 +25,14 @@ public class BlenderRender extends SimpleApplication {
     private String csvName;
 	public static void main(String args[]) {
 		BlenderRender app = new BlenderRender();
+        System.out.println("number of args = "+ args.length);
+        if (args.length < 1) {
+            args = new String[]{"scene.csv"};
+        }
+        if (args.length != 1) {
+            throw new RuntimeException("usage: blender <csvName>");
+        }
+        app.setCsvName(args[0]);
 		app.start();
 	}
     public void setCsvName(String name) {
@@ -48,9 +58,13 @@ public class BlenderRender extends SimpleApplication {
 		rootNode.attachChild(track);
         rootNode.attachChild(player);
         */
-        loadCsv(csvName);
+        try {
+            loadCsv(csvName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// sunset light
+        // sunset light
 		DirectionalLight dl = new DirectionalLight();
 		dl.setDirection(new Vector3f(-0.1f, -0.7f, 1).normalizeLocal());
 		dl.setColor(new ColorRGBA(0.44f, 0.30f, 0.20f, 1.0f));
@@ -73,10 +87,21 @@ public class BlenderRender extends SimpleApplication {
 	public void simpleUpdate(float tpf) {
 	}
 
-    private void loadCsv(String name) {
+    private void loadCsv(String name) throws IOException {
         //InputStream is = (new FileInputStream(name));
-        addAndAttachModel("Models/basement/Basement_02.blend", 0f, 0f, 0f);
-        addAndAttachModel("Models/zan/zan_texturing.blend", 0f, 0f, 0f);
+        //CSVParser parser = CSVParser.parse(new File(name), new CSVFormat(),
+        System.out.println("Reading file: "+ name);
+        Reader in = new FileReader(name);
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        for (CSVRecord record : records) {
+            String filename = record.get(0);
+            String xs = record.get(1);
+            String ys = record.get(2);
+            String zs = record.get(3);
+            //String  = record.get("X");
+            System.out.println(filename +" | "+ xs +" | "+ ys +" | "+ zs);
+            addAndAttachModel(filename, Float.parseFloat(xs), Float.parseFloat(ys), Float.parseFloat(zs));
+        }
     }
 
     private void addAndAttachModel(String path, float x, float y, float z) {

d9afa89c22c382514169954fbcf7c0a3868ce8bf
Callum Lowcay <callum@callumscode.com>
2015-03-04 12:52:47 +1300

    gui: track AP, MP and HP

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/jfx/MainHUDController.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/jfx/MainHUDController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.Pane;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Collection;
@@ -35,6 +36,17 @@ public class MainHUDController implements Initializable {
     @Override public void initialize(URL url, ResourceBundle rb) {
     }
 
+    @FXML private ProgressBar hp;
+    @FXML private ProgressBar ap;
+    @FXML private ProgressBar mp;
+		@FXML private Label hpLabel;
+		@FXML private Label apLabel;
+		@FXML private Label mpLabel;
+		@FXML private TilePane topMenu;
+		@FXML private TilePane attackMenu;
+		@FXML private TilePane magicMenu;
+		@FXML private Pane selectedCharacter;
+
 		private GameActionListener input;
 		public void setGameInput(GameActionListener input) {
 			this.input = input;
@@ -47,29 +59,44 @@ public class MainHUDController implements Initializable {
 		) {
 		}
 
+		CharacterName currentCharacter = null;
+
+		private void setPoints(ProgressBar bar, Label label, Points points) {
+			bar.setProgress((double) points.total / (double) points.max);
+			if (label != null) label.setText(points.toString());
+		}
+
 		public void selectCharacter(CharacterInfo info) {
+			currentCharacter = info.name;
+			setPoints(hp, hpLabel, info.hp);
+			setPoints(ap, apLabel, info.ap);
+			setPoints(mp, mpLabel, info.mp);
+
+			selectedCharacter.setVisible(true);
+			topMenu.setVisible(true);
 		}
 
 		public void deselectCharacter() {
+			selectedCharacter.setVisible(false);
+			topMenu.setVisible(false);
 		}
 
-		public void updateMP(CharacterName name, Points mp) {
+		public void updateMP(CharacterName name, Points points) {
+			if (name.equals(currentCharacter)) {
+				setPoints(mp, mpLabel, points);
+			}
 		}
 
-		public void updateAP(CharacterName name, Points ap) {
+		public void updateAP(CharacterName name, Points points) {
+			if (name.equals(currentCharacter)) {
+				setPoints(ap, apLabel, points);
+			}
 		}
 		
-		public void updateHP(CharacterName name, Points hp) {
+		public void updateHP(CharacterName name, Points points) {
+			if (name.equals(currentCharacter)) {
+				setPoints(hp, hpLabel, points);
+			}
 		}
-
-    @FXML private ProgressBar hp;
-    @FXML private ProgressBar ap;
-    @FXML private ProgressBar mp;
-		@FXML private Label hpLabel;
-		@FXML private Label apLabel;
-		@FXML private Label mpLabel;
-		@FXML private TilePane topMenu;
-		@FXML private TilePane AttackMenu;
-		@FXML private TilePane MagicMenu;
-    
 }
+

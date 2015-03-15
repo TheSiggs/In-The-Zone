fa9b6d587760a0ae3560271f392f5965d501718c
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-03-15 23:02:50 +1300

    Issue #30 - Converting blend2j3o.
    Simplest possible convert done.

---
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/Blend2J3o.java
@@ -0,0 +1,67 @@
+package nz.dcoder.inthezone;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.asset.AssetInfo;
+import com.jme3.asset.AssetKey;
+import com.jme3.asset.ModelKey;
+import com.jme3.asset.plugins.UrlAssetInfo;
+import com.jme3.export.binary.BinaryExporter;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.plugins.blender.BlenderModelLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by denz on 15/03/15.
+ */
+public class Blend2J3o extends SimpleApplication {
+    private final BinaryExporter binaryExporter;
+
+    public Blend2J3o() {
+        this.binaryExporter = new BinaryExporter();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        String filename = "file:///var/projects/dcoder.nz/in-the-zone/TerrainBattle/assets/Models/zan/zan_texturing.blend";
+        BlenderModelLoader loader = new BlenderModelLoader();
+        ModelKey assetKey = new ModelKey("hello");
+        URL url = null;
+        try {
+            url = new URL(filename);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        UrlAssetInfo info = null;
+        try {
+            info = UrlAssetInfo.create(assetManager, assetKey, url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Spatial spatial = null;
+        try {
+            spatial = loader.load(info);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file = new File("output.j3o");
+        try {
+            getBinaryExporter().save(spatial, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        Blend2J3o main = new Blend2J3o();
+        main.start();
+    }
+
+    public BinaryExporter getBinaryExporter() {
+        return binaryExporter;
+    }
+}

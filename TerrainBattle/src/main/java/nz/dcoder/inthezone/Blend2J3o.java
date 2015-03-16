b48b2e2b90a2f1bf82fcafff5a48e405a130f92f
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-03-16 22:21:58 +1300

    Issue #30 - Simple converter complete.
    Still need to build it into the build
    system.

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/Blend2J3o.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/Blend2J3o.java
@@ -1,56 +1,35 @@
 package nz.dcoder.inthezone;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.asset.AssetInfo;
-import com.jme3.asset.AssetKey;
-import com.jme3.asset.ModelKey;
-import com.jme3.asset.plugins.UrlAssetInfo;
+import com.jme3.asset.plugins.FileLocator;
 import com.jme3.export.binary.BinaryExporter;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.plugins.blender.BlenderModelLoader;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Created by denz on 15/03/15.
  */
 public class Blend2J3o extends SimpleApplication {
-    private final BinaryExporter binaryExporter;
+    protected final BinaryExporter binaryExporter;
+    protected String sourceBlend;
+    protected String targetJ3o;
 
     public Blend2J3o() {
-        this.binaryExporter = new BinaryExporter();
+        this.binaryExporter = BinaryExporter.getInstance();
     }
 
-    @Override
-    public void simpleInitApp() {
-        String filename = "file:///var/projects/dcoder.nz/in-the-zone/TerrainBattle/assets/Models/zan/zan_texturing.blend";
-        String assetFilename = "Models/zan/zan_texturing.blend";
-        BlenderModelLoader loader = new BlenderModelLoader();
-        ModelKey assetKey = new ModelKey("");
-        URL url = null;
-        try {
-            url = new URL(filename);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        UrlAssetInfo info = null;
-        try {
-            info = UrlAssetInfo.create(assetManager, assetKey, url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Spatial spatial = null;
-        try {
-            spatial = loader.load(info);
-            spatial = assetManager.loadModel(assetFilename);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void init() {
+        assetManager.registerLocator("assets", FileLocator.class);
+        assetManager.registerLocator("", FileLocator.class);
+    }
+
+    public void loadAndSave() {
+        Spatial spatial = assetManager.loadModel(sourceBlend);
         rootNode.attachChild(spatial);
-        File file = new File("zan_texturing.j3o");
+        File file = new File(targetJ3o);
         try {
             getBinaryExporter().save(spatial, file);
         } catch (IOException e) {
@@ -58,12 +37,51 @@ public class Blend2J3o extends SimpleApplication {
         }
     }
 
-    public static void main(String args[]) {
-        Blend2J3o main = new Blend2J3o();
-        main.start();
+    @Override
+    public void simpleInitApp() {
+        init();
+        loadAndSave();
+        this.stop();
     }
 
+    public static void main(String args[]) {
+        Blend2J3o main = new Blend2J3o();
+        if (args.length == 1) {
+            String sourceCsvFilename = args[0];
+        } else if (args.length != 2) {
+            args = new String[]{"Models/zan/zan_texturing.blend", "zan.j3o"};
+        }
+        if (args.length == 2) {
+            main.convert(args[0], args[1]);
+        } else {
+            // csv file load
+
+        }
+    }
+
+    public void convert(String blend, String toJ3o) {
+        this.setSourceBlend(blend);
+        this.setTargetJ3o(toJ3o);
+        this.setShowSettings(false);
+        this.start();
+    }
     public BinaryExporter getBinaryExporter() {
         return binaryExporter;
+    }
+
+    public String getSourceBlend() {
+        return sourceBlend;
+    }
+
+    public void setSourceBlend(String sourceBlend) {
+        this.sourceBlend = sourceBlend;
+    }
+
+    public String getTargetJ3o() {
+        return targetJ3o;
+    }
+
+    public void setTargetJ3o(String targetJ3o) {
+        this.targetJ3o = targetJ3o;
     }
 }

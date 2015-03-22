5abb9c2c6092222416de897844dbe87b4806c50f
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2015-03-22 13:56:39 +1300

    Changes from laptop.
    Not sure if it's stable.

--- TerrainBattle/src/main/java/nz/dcoder/inthezone/Blend2J3o.java
+++ TerrainBattle/src/main/java/nz/dcoder/inthezone/Blend2J3o.java
@@ -5,9 +5,11 @@ import com.jme3.asset.plugins.FileLocator;
 import com.jme3.export.binary.BinaryExporter;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.plugins.blender.BlenderModelLoader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by denz on 15/03/15.
@@ -16,6 +18,7 @@ public class Blend2J3o extends SimpleApplication {
     protected final BinaryExporter binaryExporter;
     protected String sourceBlend;
     protected String targetJ3o;
+    protected String csvFilename;
 
     public Blend2J3o() {
         this.binaryExporter = BinaryExporter.getInstance();
@@ -46,16 +49,35 @@ public class Blend2J3o extends SimpleApplication {
 
     public static void main(String args[]) {
         Blend2J3o main = new Blend2J3o();
+        String sourceCsvFilename = null;
         if (args.length == 1) {
-            String sourceCsvFilename = args[0];
-        } else if (args.length != 2) {
+            sourceCsvFilename = args[0];
+        }
+        if (args.length == 0) {
             args = new String[]{"Models/zan/zan_texturing.blend", "zan.j3o"};
         }
         if (args.length == 2) {
             main.convert(args[0], args[1]);
-        } else {
+        }
+        if (sourceCsvFilename != null) {
             // csv file load
+            try {
+                main.loadCsv(sourceCsvFilename);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private void loadCsv(String sourceCsvFilename) throws IOException {
+        this.csvFilename = sourceCsvFilename;
+        Reader in = new FileReader(sourceCsvFilename);
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        for (CSVRecord record : records) {
+            String filename = record.get(0);
+            String toJ3o = filename.replace(".blend", ".j3o");
+            this.
+            //convert(filename, toJ3o);
         }
     }
 

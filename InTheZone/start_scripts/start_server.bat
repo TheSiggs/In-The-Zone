7d9d46ffdd3f4b1a1c5c8a28e2a35edec76f9b5b
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2016-09-11 21:34:51 +1200

    windows batch files for startup - not tested

--- InTheZone/start_scripts/start_server.bat
+++ InTheZone/start_scripts/start_server.bat
@@ -1,3 +1,7 @@
 @echo off
-set JAVA_HOME=jre/win_64
-bin\server.bat %CMD_LINE_ARGS%
+REM FOR LATER:
+REM set JAVA_HOME=jre/win_64
+SET mypath=%~dp0
+SET DIR=%mypath:~0,-1%
+REM $DIR\bin\server.bat %CMD_LINE_ARGS%
+$DIR\bin\server.bat port=8000 basedir="$DIR\gamedata"

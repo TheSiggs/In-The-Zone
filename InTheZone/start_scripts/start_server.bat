4a30d3156617406d77a483ff9eb10b2d5e56dd06
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2016-09-13 12:09:29 +1200

    windows startup scripts working and tested on Windows 7

--- InTheZone/start_scripts/start_server.bat
+++ InTheZone/start_scripts/start_server.bat
@@ -4,4 +4,5 @@ REM set JAVA_HOME=jre/win_64
 SET mypath=%~dp0
 SET DIR=%mypath:~0,-1%
 REM $DIR\bin\server.bat %CMD_LINE_ARGS%
-$DIR\bin\server.bat port=8000 basedir="$DIR\gamedata"
+echo %DIR%
+%DIR%\bin\server.bat port=8000 basedir="%DIR%\gamedata"

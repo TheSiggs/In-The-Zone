984e2eaa203ef91724296d3b2345340583e082b3
Tim-Hinnerk Heuer <th.heuer@gmail.com>
2016-09-16 23:20:31 +1200

    #18 Better native package building - Linux.

--- InTheZone/build_dmgs.sh
+++ InTheZone/build_dmgs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-jdk=$(/usr/libexec/java_home)
+jdk=$(echo $JAVA_HOME || /usr/libexec/java_home)
 $jdk/bin/javapackager -deploy -native dmg -outdir build/deploy \
 	-outfile InTheZone.app -srcfiles server/build/libs/server-all.jar \
 	-appclass inthezone.server.Server -title InTheZoneServer \

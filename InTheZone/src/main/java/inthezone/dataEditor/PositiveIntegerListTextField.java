b49cfce3c8ab53a431f51d22ae716ac0931afe12
Callum Lowcay <callum@callumscode.com>
2016-11-28 21:18:50 +0900

    new kind of text field for character stat curves

---
+++ InTheZone/src/main/java/inthezone/dataEditor/PositiveIntegerListTextField.java
@@ -0,0 +1,49 @@
+package inthezone.dataEditor;
+
+import isogame.gui.TypedTextField;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PositiveIntegerListTextField extends TypedTextField<List<Integer>> {
+	@Override protected Optional<List<Integer>> parseValue(String t) {
+		try {
+			String[] ts = t.split("\\s*,\\s*");
+			List<Integer> r = new ArrayList<>();
+			for (String s : ts) {
+				r.add(Integer.parseUnsignedInt(t));
+			}
+			return Optional.of(r);
+
+		} catch (NumberFormatException e) {
+			return Optional.empty();
+		}
+	}
+
+	@Override protected List<Integer> getDefaultValue() {return new ArrayList<>();}
+
+	@Override protected String showValue(List<Integer> is) {
+		StringBuilder out = new StringBuilder();
+		boolean first = true;
+		for (Integer i : is) {
+			if (!first) out.append(",");
+			first = false;
+			out.append(i.toString());
+		}
+
+		return out.toString();
+	}
+
+	public PositiveIntegerListTextField(List<Integer> init) {
+		super(init);
+	}
+
+	public PositiveIntegerListTextField(String text) {
+		super(text);
+	}
+
+	public PositiveIntegerListTextField() {
+		super();
+	}
+}
+

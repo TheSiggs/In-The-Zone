6414a441669bf3db528ff2655ec6cc007c40b506
Callum Lowcay <callum@callumscode.com>
2017-06-10 14:15:15 +0900

    more precise targeting

--- InTheZone/src/main/java/inthezone/comptroller/InfoTargeting.java
+++ InTheZone/src/main/java/inthezone/comptroller/InfoTargeting.java
@@ -2,9 +2,13 @@ package inthezone.comptroller;
 
 import inthezone.battle.Ability;
 import inthezone.battle.Battle;
+import inthezone.battle.Casting;
 import inthezone.battle.Character;
+import inthezone.battle.commands.AbilityAgentType;
+import inthezone.battle.data.AbilityZoneType;
 import isogame.engine.MapPoint;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,6 +33,7 @@ public class InfoTargeting extends InfoRequest<Collection<MapPoint>> {
 		this.retargetedFrom = retargetedFrom;
 	}
 
+
 	@Override public void completeAction(Battle battle) {
 		final Collection<MapPoint> area =
 			battle.battleState.getTargetableArea(
@@ -43,9 +48,44 @@ public class InfoTargeting extends InfoRequest<Collection<MapPoint>> {
 					!battle.battleState.getTrapAt(p).isPresent() &&
 					!battle.battleState.hasDefusingZone(p))
 				.collect(Collectors.toList()));
-		}
 
-		complete.complete(area);
+		} else if (
+			ability.info.instantBefore.map(i -> i.isField()).orElse(false) ||
+			ability.info.instantAfter.map(i -> i.isField()).orElse(false)
+		) {
+			complete.complete(area.stream()
+				.filter(p -> battle.battleState.isSpaceFree(p))
+				.collect(Collectors.toList()));
+
+		} else if (ability.info.zone == AbilityZoneType.ZONE) {
+			complete.complete(area.stream()
+				.filter(p ->
+					!battle.battleState.getAffectedArea(
+						subject.getPos(), AbilityAgentType.CHARACTER,
+						ability, new Casting(castFrom, p)).stream()
+					.anyMatch(p2 -> battle.battleState.getZoneAt(p2).isPresent()))
+				.collect(Collectors.toList()));
+
+		} else if (ability.info.zone == AbilityZoneType.BOUND_ZONE) {
+			complete.complete(area.stream()
+				.filter(p ->
+					battle.battleState.isSpaceFree(p) &&
+					!battle.battleState.getAffectedArea(
+						subject.getPos(), AbilityAgentType.CHARACTER,
+						ability, new Casting(castFrom, p)).stream()
+					.anyMatch(p2 -> battle.battleState.getZoneAt(p2).isPresent()))
+				.collect(Collectors.toList()));
+			
+		} else {
+			complete.complete(area.stream()
+				.filter(t -> 
+					!battle.battleState.getAbilityTargets(
+						subject.getPos(), AbilityAgentType.CHARACTER,
+						ability, battle.battleState.getAffectedArea(
+							subject.getPos(), AbilityAgentType.CHARACTER,
+							ability, new Casting(castFrom, t))).isEmpty())
+				.collect(Collectors.toList()));
+		}
 	}
 }
 

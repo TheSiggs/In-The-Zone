ac98cb2c323f9d49b67a17c68c99943287ee0790
jedc <jedcalkin@gmail.com>
2017-07-16 01:32:35 +0000

    deploy server

--- deploy/GitLabHook.sh
+++ deploy/GitLabHook.sh
@@ -10,5 +10,7 @@ cp ./client/build/libs/* ./client/build/libs/client-latest.jar
 cp ./client/build/libs/* ../../
 
 
+cp ./server/build/libs/* ./server/build/libs/server-latest.jar
+cp ./server/build/libs/* /home/itz/server/
 
 
